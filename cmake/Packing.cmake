3fa9bb4dc1fcab46f5c07d87abb9adfe6792e0cf
retif <retifrav@gmail.com>
2021-09-22 22:21:44 +0200

    Feng-shui

--- cmake/Packing.cmake
+++ cmake/Packing.cmake
@@ -1,9 +1,11 @@
 # these are cache variables, so they could be overwritten with -D,
-set(CPACK_PACKAGE_NAME ${PROJECT_NAME}
+set(
+    CPACK_PACKAGE_NAME ${PROJECT_NAME}
     CACHE STRING "The resulting package name"
 )
 # which is useful in case of packing only selected components instead of the whole thing
-set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Simple C++ application"
+set(
+    CPACK_PACKAGE_DESCRIPTION_SUMMARY "Simple C++ application"
     CACHE STRING "Package description for the package metadata"
 )
 set(CPACK_PACKAGE_VENDOR "Some Company")
@@ -14,14 +16,14 @@ set(CPACK_PACKAGE_INSTALL_DIRECTORY ${CPACK_PACKAGE_NAME})
 SET(CPACK_OUTPUT_FILE_PREFIX "${CMAKE_SOURCE_DIR}/_packages")
 set(CPACK_STRIP_FILES YES)
 
-set(CPACK_INSTALL_DEFAULT_DIRECTORY_PERMISSIONS
+set(
+    CPACK_INSTALL_DEFAULT_DIRECTORY_PERMISSIONS
     OWNER_READ OWNER_WRITE OWNER_EXECUTE
     GROUP_READ GROUP_EXECUTE
     WORLD_READ WORLD_EXECUTE
 )
 
-# you might want to drop version from this path
-set(CPACK_PACKAGING_INSTALL_PREFIX "/opt/some/${CMAKE_PROJECT_VERSION}")
+set(CPACK_PACKAGING_INSTALL_PREFIX "/opt/some")#/${CMAKE_PROJECT_VERSION}")
 
 set(CPACK_PACKAGE_VERSION_MAJOR ${PROJECT_VERSION_MAJOR})
 set(CPACK_PACKAGE_VERSION_MINOR ${PROJECT_VERSION_MINOR})
@@ -34,8 +36,8 @@ set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE")
 set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
 
 # package name for deb
-# if set, then instead of SomeLibrary-0.9.1-Darwin.deb
-# you'll get somelibrary_0.9.1_darwin-amd64.deb
+# if set, then instead of some-application-0.9.2-Linux.deb
+# you'll get some-application_0.9.2_amd64.deb (note the underscores too)
 set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
 # if you want every group to have its own package,
 # although the same happens if this is not sent (so it defaults to ONE_PER_GROUP)
