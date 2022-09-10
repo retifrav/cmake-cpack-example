1896db69c10f598562919abb2a7eb15e0a63af4b
retif <retifrav@gmail.com>
2022-09-10 11:42:04 +0200

    Typos

--- cmake/Packing.cmake
+++ cmake/Packing.cmake
@@ -35,12 +35,11 @@ set(CPACK_DEBIAN_PACKAGE_MAINTAINER "YOUR NAME <${CPACK_PACKAGE_CONTACT}>")
 set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE")
 set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
 
-# package name for deb
-# if set, then instead of some-application-0.9.2-Linux.deb
+# package name for deb. If set, then instead of some-application-0.9.2-Linux.deb
 # you'll get some-application_0.9.2_amd64.deb (note the underscores too)
 set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
-# if you want every group to have its own package,
-# although the same happens if this is not sent (so it defaults to ONE_PER_GROUP)
+# that is if you want every group to have its own package,
+# although the same will happen if this is not set (so it defaults to ONE_PER_GROUP)
 # and CPACK_DEB_COMPONENT_INSTALL is set to YES
 set(CPACK_COMPONENTS_GROUPING ALL_COMPONENTS_IN_ONE)#ONE_PER_GROUP)
 # without this you won't be able to pack only specified component
